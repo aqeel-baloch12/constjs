a852dcb2375e332ffc275cef34f8d9806c391406
aqeel-baloch12 <aqeelsuleman310@gmail.com>
2024-09-11 02:06:53 -0700

    adding js

---
+++ script.js
@@ -0,0 +1,12 @@
+const pi=3.14
+let radius=4;
+console.log(pi*radius*radius);
+
+
+
+const myName='AQEEL';
+console.log(myName)
+document.querySelector('#bro').innerText=myName;
+
+
+console.log(eval('2+3*4'));
